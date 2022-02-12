d3db2d49dd9b528db793d612a4ba5e06f58378af
ბაჩო ბექაური <81110694+200MB@users.noreply.github.com>
2022-02-12 17:45:57 +0400

    need Fx setup

--- src/main/java/SQLConnections/Info.java
+++ src/main/java/SQLConnections/Info.java
@@ -1,7 +1,7 @@
 package SQLConnections;
 
 public class Info {
-    public static final String HOST= "jdbc:mysql://localhost:3306";
+    public static final String HOST= "jdbc:mysql://localhost:3306/skola";
     public static final String USER = "root";
     public static final String PASSWORD = "root";
 }
--- src/main/java/Startup/Startup.java
+++ src/main/java/Startup/Startup.java
@@ -21,19 +21,19 @@ public class Startup {
         PreparedStatement p = con.prepareStatement("use skola");
         p.execute();
         if (!tableCheck("grades")) {
-            System.out.println("DEBUG: created Table 'GRADES'");
             p = con.prepareStatement("CREATE TABLE `skola`.`grades` (`UserID` INT NOT NULL,`Georgian` VARCHAR(45) NULL,`History` VARCHAR(45) NULL,`Math` VARCHAR(45) NULL,`Physics` VARCHAR(45) NULL,`Biology` VARCHAR(45) NULL,`Chemistry` VARCHAR(45) NULL,`Geography` VARCHAR(45) NULL,`English` VARCHAR(45) NULL,`Russian` VARCHAR(45) NULL,PRIMARY KEY (`UserID`));");
             p.execute();
+            System.out.println("DEBUG: created Table 'GRADES'");
         }
         if (!tableCheck("user")) {
-            System.out.println("DEBUG: created Table 'USER'");
-            p = con.prepareStatement("CREATE TABLE `skola`.`user` (`UserID` INT NOT NULL,`Name` VARCHAR(45) NOT NULL,`Lastname` VARCHAR(45) NOT NULL,PRIMARY KEY (`UserID`));");
+            p = con.prepareStatement("CREATE TABLE `skola`.`user` (`UserID` INT NOT NULL,`Name` VARCHAR(45) NOT NULL,`Lastname` VARCHAR(45) NOT NULL,`Email` VARCHAR(45) NOT NULL,`Class` VARCHAR(45) NOT NULL,`Subject` VARCHAR(45) NOT NULL,`isStudent` BOOLEAN NOT NULL,PRIMARY KEY (`UserID`));");
             p.execute();
+            System.out.println("DEBUG: created Table 'USER'");
         }
         if (!tableCheck("class")) {
-            System.out.println("DEBUG: created Table 'CLASS'");
-            p = con.prepareStatement("CREATE TABLE `skola`.`class` (`ClassId` INT NOT NULL AUTO_INCREMENT,`ClassUserId` INT NOT NULL,`ClassTeachId` INT NOT NULL,PRIMARY KEY (`ClassId`));");
+            p = con.prepareStatement("CREATE TABLE `skola`.`class` (`ClassId` INT NOT NULL AUTO_INCREMENT,`ClassUserId` INT NOT NULL,`ClassTeachId` INT NOT NULL,`ClassGrade` INT NOT NULL,PRIMARY KEY (`ClassId`));");
             p.execute();
+            System.out.println("DEBUG: created Table 'CLASS'");
         }
 
     }
@@ -41,6 +41,3 @@ public class Startup {
 
 }
 
-//DONE: create grade table
-//DONE: create user table
-//DONE: create class table
